d3ecdf0c8a7d505c06648e93ebbe7f5466dd8e24
Sunil <sunhick@gmail.com>
2018-03-14 22:38:44 -0700

    Formatting the code

--- haskell/intro/src/Graphics/Shape.hs
+++ haskell/intro/src/Graphics/Shape.hs
@@ -1,14 +1,10 @@
-module Graphics.Shape (          
-               Circle   
-             , Rectangle
-             , Triangle 
-             , Rhombus  
-             ) where
+module Graphics.Shape ( Circle
+                      , Rectangle
+                      , Triangle
+                      , Rhombus
+                      ) where
 
 data Circle = Circle String String Int deriving (Show)
-
 data Rectangle = Rectangle String String Int deriving (Show)
-
 data Triangle = Triangle String String Int deriving (Show)
-
 data Rhombus = Rhombus String String Int deriving (Show)
