df4199e049e3c8c8ce27a14a35eb5533276f6265
Sunil <sunhick@gmail.com>
2018-07-22 18:22:36 -0700

    javascript bootstrap

---
+++ javascript/hello/hello.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let colors = require('colors')
+
+let message = "hello world in javascript";
+
+const kAlpha = 6.6;
+
+console.log(message.green);
+console.log(message.yellow);
+console.log(message.red);
+console.log(message);
